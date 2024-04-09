3f9bad8963565d32f73d29c22cd99a13713b13e7
12Ahn22 <12ayo22@gmail.com>
2024-04-09 19:00:15 +0900

    [init]

---
+++ my-mini1/src/main/java/org/mini/proj/MyMini1Application.java
@@ -0,0 +1,13 @@
+package org.mini.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMini1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMini1Application.class, args);
+	}
+
+}
---
+++ my-mini1/src/test/java/org/mini/proj/MyMini1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mini.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMini1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
